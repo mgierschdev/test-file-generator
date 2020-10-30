1b77d7902a99b383d2624f213802d390076b1b13
Maximiliam <mgierschmann@protonmail.com>
2020-10-30 11:29:57 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # generate-file
-Generate different files types (txt, csv, img and other formats), for testing purposes in node.js
+(Under development)
+Generate different file types (txt, csv, img and other formats), for testing purposes in node.js
---
+++ index.js
@@ -0,0 +1,11 @@
+fs = require('fs');
+
+exports.myDateTime = function () {
+  return Date();
+};
+
+//
+// fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
+//   if (err) return console.log(err);
+//   console.log('Hello World > helloworld.txt');
+// });
