7f746369959fa6c1336ee67a6d2310a6fe3fbcba
Maximiliam <mgierschmann@protonmail.com>
2020-12-01 15:11:41 -0400

    Adding yargs support

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import yargs = require('yargs/yargs');
+import { TestFileGenerator } from './classes/TestFileGenerator';
+
+type fileType = 'jpeg' | 'gif' | 'cvs' | 'txt';
+const filesTypes: ReadonlyArray<fileType> = ['jpeg', 'gif', 'cvs', 'txt'];
+
+const argv = yargs(process.argv.slice(2))
+  .usage('Usage: generate -type gif -count 1')
+  .options({
+    path: { type: 'string', default: './', description: 'default ./', alias: 'path' },
+    type: { choices: filesTypes, default: 'jpeg' },
+  }).argv;
+
+const generator = new TestFileGenerator(argv.type);
+generator.setLocation(argv.path);
+generator.generateFile();
